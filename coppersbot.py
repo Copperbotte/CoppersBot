413268b6b4c5651b0125595f282f790974f948fc
Copperbotte <copperbotte@gmail.com>
2020-04-06 19:12:07 -0700

    implemented google sheets lookup

--- coppersbot.py
+++ coppersbot.py
@@ -1,10 +1,41 @@
 
+import os
 import discord
+
+import pickle
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
 USERID = '<@!490655530172547073>'
 client = discord.Client()
+
+def getData():
+    spreadsheet = "10zcHJfs8IV1IiN2JWEyPDluZhxsD7HNp0KU1WMfHRvw"
+    ssrange = "B1:N2"
+    creds = None
+    print("creds")
+    picklepath = r'token.pickle'
+    if os.path.exists(picklepath):
+        with open(picklepath, 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(r'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open(picklepath, 'wb') as token:
+            pickle.dump(creds, token)
+    print("service")
+    service = build('sheets', 'v4', credentials=creds)
+    print("sheet")
+    sheet = service.spreadsheets()
+    print("get")
+    result = sheet.values().get(spreadsheetId=spreadsheet, range=ssrange).execute()
+    return result.get('values', [])
 
 @client.event
 async def on_message(message):
@@ -25,7 +56,17 @@ async def on_message(message):
         await message.channel.send("quitting")
         await client.logout()
         return
-		
+
+    if cmd == "get":
+        data = getData()
+        msg = ""
+        for row in data:
+            for col in row:
+                msg += str(col) + ' '
+            msg += '\n'
+        await message.channel.send(msg)
+        return
+    
 		
 @client.event
 async def on_ready():
